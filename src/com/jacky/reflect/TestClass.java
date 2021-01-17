1fb1d13e2910963a64926f5188eb1c9fac8c074a
Jacky <964413011@qq.com>
2021-01-17 17:24:47 +0800

    反射功能测试

---
+++ src/com/jacky/reflect/TestClass.java
@@ -0,0 +1,6 @@
+package com.jacky.reflect;
+
+public class TestClass {
+    public int a=0;
+    private int b=11;
+}
---
+++ src/com/jacky/reflect/reflect.java
@@ -0,0 +1,10 @@
+package com.jacky.reflect;
+
+public class reflect {
+    public static void main(String[] args) {
+        Class class1=TestClass.class;
+
+        System.out.println(class1.getName());
+        System.out.println(class1.toString());
+    }
+}
